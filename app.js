6b93bbc201fbddd3bc4176484869b399506eb55b
AndreCorrea <andreff55@gmail.com>
2023-09-24 21:10:36 -0300

    updates

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ script.onload = function(){
 			var whatsappUrl;
 			const response = await axios.post('http://localhost:5000/leads', data);
 
-			if(!response){
+			if(!response.data && !response.data.whatsapp){
 				h.text('Insira as informações para iniciar uma conversa')
 				h.removeClass('loading');
 				f.fadeIn('slow/400/fast');
